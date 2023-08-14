15cc612b44da664e9feaa275829385bbbd84a08f
RedsonBr140 <redson@riseup.net>
2023-08-14 19:31:58 -0300

    feat: Authorization (#1)
    * feat: Authentication system
    
    * feat: dotenv file
    
    * feat: Authorization Middleware

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 import express from "express";
+import "dotenv/config";
 
 const app = express();
+app.use(express.json());
+
+// Controllers
+import { register, login, tokenMiddleware } from "./controllers/auth.js";
+
+app.get("/", (req, res) => {
+    res.json({msg: "Hello, World!"});
+});
+
+app.get("/authorized", tokenMiddleware, (req, res) => {
+    res.json({msg: "Authorized!"});
+});
+
+app.post("/auth/register", register);
+app.post("/auth/login", login);
 
 app.listen(3000, function () {
     console.log("Server running in localhost:3000");
